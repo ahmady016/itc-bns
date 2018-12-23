bd08ebedb720889f370d8a16580a8163ac8a87dc
ahmady016 <ahmady09@gmail.com>
2018-12-23 02:27:52 +0200

    Make the NavBar and Admin Routes dynamic

--- src/views/layout/Admin.js
+++ src/views/layout/Admin.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Route, Switch } from 'react-router-dom'
-import { dbActions } from '../../redux/db'
-import NavBar from './NavBar';
-import Register from '../public/Register'
-import Login from '../public/Login'
-import ChangePassword from '../admin/ChangePassword'
-import RegisterUser from '../public/RegisterUser';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { dbActions } from "../../redux/db";
+import NavBar from "./NavBar";
+import Register from "../public/Register";
+import Login from "../public/Login";
+import ChangePassword from "../admin/ChangePassword";
+import RegisterUser from "../public/RegisterUser";
 
 class Admin extends Component {
+  constructor(props) {
+    super(props);
+    // extract current url from routes props
+    const { match: { url } } = this.props;
+    // the default Route Path to Redirect to
+    this.defaultPath = `${url}/login`;
+    // the Nav Links [Path - text - component]
+    this.navLinks = [
+      { path: `${url}/change-password`, text: "تعديل كلمة المرور", component: ChangePassword },
+      { path: `${url}/register-user`, text: "تسجيل حساب مستخدم", component: RegisterUser },
+      { path: `${url}/register`, text: "تسجيل حساب", component: Register },
+      { path: `${url}/login`, text: "تسجيل دخول", component: Login }
+    ];
+  }
   componentDidMount() {
     // add all realtime updates listeners
     // dbActions.mountListeners([
@@ -32,13 +46,11 @@ class Admin extends Component {
   render() {
     return (
       <>
-        <NavBar />
+        <NavBar links={this.navLinks} />
         <div className="container">
           <Switch>
-            <Route path="/admin/change-password"  component={ChangePassword} />
-            <Route path="/admin/register-user"    component={RegisterUser} />
-            <Route path="/admin/register"         component={Register} />
-            <Route path="/admin/"                 component={Login} />
+            {this.navLinks.map(link => <Route path={link.path} component={link.component} /> )}
+            <Redirect to={this.defaultPath} />
           </Switch>
         </div>
       </>
@@ -46,7 +58,7 @@ class Admin extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   ...state.db
 });
 
--- src/views/layout/NavBar.js
+++ src/views/layout/NavBar.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
-export default function NavBar() {
+export default function NavBar({ links }) {
+  let url = window.location.href;
+  url = url.slice(url.lastIndexOf('/')+1);
   const setActiveNav = (path) => {
-    const url = window.location.href;
-    const _url = url.slice(url.lastIndexOf('/')+1);
-    const _path = path.slice(path.lastIndexOf('/')+1);
-    return (_url === _path) ? 'active' : '';
+    path = path.slice(path.lastIndexOf('/')+1);
+    return (url === path) ? 'active' : '';
   }
-  const links = [
-    { path: "/admin/change-password", text: "تعديل كلمة المرور" },
-    { path: "/admin/register-user", text: "تسجيل حساب مستخدم" },
-    { path: "/admin/register", text: "تسجيل حساب" },
-    { path: "/admin/", text: "تسجيل دخول" }
-  ];
   return (
     <nav>
       <div className="nav-wrapper">
